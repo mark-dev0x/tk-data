30ae483ab447c785e0c18ce455628dfa22c14b59
mark-dev0x <markmella.dev@gmail.com>
2025-08-14 05:00:17 +0000

    up

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,7 @@
         <form class="space-y-8 w-full" @submit.prevent="handleLogin">
           <div
             v-if="error"
-            class="bg-[#d14124] bg-opacity-80 border border-red-400 text-white px-4 py-3 rounded-lg mb-4"
+            class="bg-[#d14124] text-center bg-opacity-80 border border-red-400 text-white px-4 py-3 rounded-lg mb-4"
           >
             {{ error }}
           </div>
